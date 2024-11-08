ebc0b2c4f5aa4058b254e0ac5bae9feeaba98b7c
cristianorcjrl <cristianorcjrl@users.noreply.github.com>
2024-11-08 22:38:02 +0000

    deploy: 38b166eb7704bec32066bb9dd906853a6d506df0

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// JavaScript para el menú de navegación móvil
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('nav-open');
+});
+
+// Manejar el envío del formulario de contacto con EmailJS y SweetAlert
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    emailjs.send("service_tdree0d", "template_8rp8k7m", {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    }).then(function(response) {
+        Swal.fire({
+            title: 'Mensaje enviado',
+            text: 'Nos pondremos en contacto pronto.',
+            icon: 'success',
+            confirmButtonText: 'Aceptar'
+        });
+        document.getElementById("contact-form").reset();
+    }, function(error) {
+        Swal.fire({
+            title: 'Error',
+            text: 'No se pudo enviar el mensaje. Intenta de nuevo.',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+        });
+    });
+});
