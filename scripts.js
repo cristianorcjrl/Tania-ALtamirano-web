f25ba6cc8af5d6966153abc47ad078d4fbdc08ba
cristianorcjrl <cristianorcjrl@users.noreply.github.com>
2024-11-10 15:45:57 +0000

    deploy: 3e437986b5080050a62b4707e86fe209132c02a9

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+// Inicializar EmailJS con la clave pública
+emailjs.init("xEA2aBIqVj198adgp"); // Reemplaza "YOUR_PUBLIC_KEY" con tu clave pública de EmailJS
+
 // JavaScript para el menú de navegación móvil
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('nav').classList.toggle('nav-open');
@@ -7,24 +10,22 @@ document.querySelector('.menu-toggle').addEventListener('click', function() {
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    emailjs.send("service_tdree0d", "template_8rp8k7m", {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    }).then(function(response) {
-        Swal.fire({
-            title: 'Mensaje enviado',
-            text: 'Nos pondremos en contacto pronto.',
-            icon: 'success',
-            confirmButtonText: 'Aceptar'
+    emailjs.sendForm('service_tdree0d', 'template_8rp8k7m', this)
+        .then(function(response) {
+            Swal.fire({
+                title: 'Mensaje enviado',
+                text: 'Nos pondremos en contacto pronto.',
+                icon: 'success',
+                confirmButtonText: 'Aceptar'
+            });
+            document.getElementById("contact-form").reset();
+        }, function(error) {
+            Swal.fire({
+                title: 'Error',
+                text: 'No se pudo enviar el mensaje. Intenta de nuevo.',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            });
+            console.error('Error al enviar el formulario:', error);
         });
-        document.getElementById("contact-form").reset();
-    }, function(error) {
-        Swal.fire({
-            title: 'Error',
-            text: 'No se pudo enviar el mensaje. Intenta de nuevo.',
-            icon: 'error',
-            confirmButtonText: 'Aceptar'
-        });
-    });
 });
